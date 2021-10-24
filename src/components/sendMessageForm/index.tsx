a6e87fec2acf7823b7538aaa1e61b8e508aab607
rwellingtonr <wellington-158@hotmail.com>
2021-10-24 18:44:26 -0300

    Styling the send message form

--- src/components/sendMessageForm/index.tsx
+++ src/components/sendMessageForm/index.tsx
@@ -1,3 +1,34 @@
+import { useContext } from "react"
+import { VscGithubInverted, VscSignOut } from "react-icons/vsc"
+import { AuthContext } from "../../context/auth"
+import styles from "./styles.module.scss"
+
 export const SendMessageForm = () => {
-  return <h1>SendMessageForm</h1>
+  const { user } = useContext(AuthContext)
+
+  return (
+    <div className={styles.sendMessageFormWrapper}>
+      <button className={styles.signOutButton}>
+        <VscSignOut size="32" />
+      </button>
+      <header className={styles.userInformation}>
+        <div className={styles.userImage}>
+          <img src={user?.avatar_url} alt={user?.name} />
+        </div>
+        <strong className={styles.userName}>{user?.name}</strong>
+        <span className={styles.userGitHub}>
+          <VscGithubInverted size="16" />
+          {user?.login}
+        </span>
+      </header>
+
+      <form className={styles.sendMessageForm}>
+        <label htmlFor="message">Mensagem</label>
+        <textarea name="message" id="message" placeholder="Mensagem">
+          Qual é a sua expectativa para o evento?
+        </textarea>
+        <button type="submit">Enviar mensagem</button>
+      </form>
+    </div>
+  )
 }
